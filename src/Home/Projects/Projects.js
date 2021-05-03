6d2a06e1cd4d38ca4b624d527d201deb0d32b690
Abdul Aziz <aziz12d97@gmail.com>
2021-05-03 13:11:44 +0600

    scroll bar and projects update

--- src/Home/Projects/Projects.js
+++ src/Home/Projects/Projects.js
@@ -3,7 +3,7 @@ import './Projects.css'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { FaBeer, FaEye } from 'react-icons/fa';
-import { DiGithubBadge,DiReact,DiNodejs,DiNpm,DiMongodb,DiFirebase,DiHeroku } from "react-icons/di";
+import { DiGithubBadge, DiReact, DiNodejs, DiNpm, DiMongodb, DiFirebase, DiHeroku } from "react-icons/di";
 import wolfSolution from '../../images/wolf-it-solution.png';
 import carmiaOnlineShop from '../../images/carmia-online-shop.png';
 import alManahilBd from '../../images/al-manahil-bd.png';
@@ -13,7 +13,11 @@ import uefaFootballTeam from '../../images/football-api.png';
 
 const Projects = () => {
     const imageStyle = {
-        margin: '10px 0px', height:'300px',width:'100%',objectFit:'cover', objectPosition:'top' 
+        margin: '10px 0px', width: '100%', objectFit: 'cover', objectPosition: 'top'
+    }
+    const imageDivStyle = {
+        height: '300px', width: '100%',  overflowY:'scroll'
+        
     }
     return (
         <div class="main-content">
@@ -23,17 +27,21 @@ const Projects = () => {
             <div class="content-body">
 
 
-                <VerticalTimeline>
+                <VerticalTimeline
+                    layout="1-column"
+                >
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
                         contentStyle={{ background: '#fff', color: 'rgb(33, 150, 243)' }}
                         contentArrowStyle={{ borderRight: '7px solid  #fff' }}
                         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        icon={<FaBeer />}
+                        icon={<DiReact />}
                     >
 
                         <h3 className="vertical-timeline-element-title">Wolf IT Solution</h3>
-                        <img style={imageStyle} src={wolfSolution} alt="" />
+                        <div style={imageDivStyle}>
+                            <img style={imageStyle} src={wolfSolution} alt="" />
+                        </div>
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>
                             Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
@@ -45,7 +53,7 @@ const Projects = () => {
                             <DiMongodb title="Mongodb" className="tech-icon" />
                             <DiHeroku title="Heroku" className="tech-icon" />
                             <DiFirebase title="Firebase" className="tech-icon" />
-                            
+
                         </div>
                         <div className='element-bottom'>
                             <a target="_blank" href="https://wolf-solution.web.app/"><FaEye className="element-bottom-icon" /> Preview</a>
@@ -55,11 +63,15 @@ const Projects = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
                         date=""
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={<DiReact />}
+                        iconStyle={{ background: '#5F9EA0', color: '#fff' }}
 
                     >
                         <h3 className="vertical-timeline-element-title">Carmia Online Shop</h3>
-                        <img style={imageStyle} src={carmiaOnlineShop} alt="" />
+                        <div style={imageDivStyle}>
+                            <img style={imageStyle} src={carmiaOnlineShop} alt="" />
+                        </div>
+
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>
                             Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
@@ -71,7 +83,7 @@ const Projects = () => {
                             <DiMongodb title="Mongodb" className="tech-icon" />
                             <DiHeroku title="Heroku" className="tech-icon" />
                             <DiFirebase title="Firebase" className="tech-icon" />
-                            
+
                         </div>
                         <div className='element-bottom'>
                             <a target="_blank" target="_blank" href="https://carmia-b18f4.web.app/"><FaEye className="element-bottom-icon" /> Preview</a>
@@ -81,11 +93,13 @@ const Projects = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
                         date=""
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        icon={<FaBeer />}
+                        iconStyle={{ background: '#343A40', color: '#fff' }}
+                        icon={<DiReact />}
                     >
                         <h3 className="vertical-timeline-element-title">Al Manahil Project Management</h3>
-                        <img style={imageStyle} src={almanahilbd2} alt="" />
+                        <div style={imageDivStyle}>
+                            <img style={imageStyle} src={almanahilbd2} alt="" />
+                        </div>
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>
                             Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
@@ -97,7 +111,7 @@ const Projects = () => {
                             <DiMongodb title="Mongodb" className="tech-icon" />
                             <DiHeroku title="Heroku" className="tech-icon" />
                             <DiFirebase title="Firebase" className="tech-icon" />
-                            
+
                         </div>
                         <div className='element-bottom'>
                             <a target="_blank" href="http://almanahilbd.com/"><FaEye className="element-bottom-icon" /> Preview</a>
@@ -107,11 +121,13 @@ const Projects = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
                         date=""
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                        icon={<FaBeer />}
+                        iconStyle={{ background: '#8A2BE2', color: '#fff' }}
+                        icon={<DiReact />}
                     >
                         <h3 className="vertical-timeline-element-title">Wolf Hunter Football League</h3>
-                        <img style={imageStyle} src={wolfHunterLeague} alt="" />
+                        <div style={imageDivStyle}>
+                            <img style={imageStyle} src={wolfHunterLeague} alt="" />
+                        </div>
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>
                             Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
@@ -123,7 +139,7 @@ const Projects = () => {
                             <DiMongodb title="Mongodb" className="tech-icon" />
                             <DiHeroku title="Heroku" className="tech-icon" />
                             <DiFirebase title="Firebase" className="tech-icon" />
-                            
+
                         </div>
                         <div className='element-bottom'>
                             <a target="_blank" href="https://objective-brown-d265be.netlify.app/"><FaEye className="element-bottom-icon" /> Preview</a>
@@ -133,11 +149,13 @@ const Projects = () => {
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
                         date="November 2012"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                        icon={<FaBeer />}
+                        iconStyle={{ background: '#2A1542', color: '#fff' }}
+                        icon={<DiReact />}
                     >
                         <h3 className="vertical-timeline-element-title">UEFA Football Team</h3>
-                        <img style={imageStyle} src={uefaFootballTeam} alt="" />
+                        <div style={imageDivStyle}>
+                            <img style={imageStyle} src={uefaFootballTeam} alt="" />
+                        </div>
                         <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                         <p>
                             Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
@@ -149,7 +167,7 @@ const Projects = () => {
                             <DiMongodb title="Mongodb" className="tech-icon" />
                             <DiHeroku title="Heroku" className="tech-icon" />
                             <DiFirebase title="Firebase" className="tech-icon" />
-                            
+
                         </div>
                         <div className='element-bottom'>
                             <a target="_blank" href="https://focused-hawking-bb782c.netlify.app/"><FaEye className="element-bottom-icon" /> Preview</a>
